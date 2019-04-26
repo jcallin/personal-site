5ecf47ac773888aa7582dc35028547e3eaeb9bc0
Julian Callin <j.callin@hotmail.com>
2019-04-26 10:04:23 -0700

    Fix cd location

--- build/build_and_test_all.sh
+++ build/build_and_test_all.sh
@@ -1,2 +1,2 @@
 (cd personal-site-api && sh ../build/test_api.sh && sh ../build/build_api.sh)
-(cd ../personal-site-frontend && sh ../build/test_fe.sh && sh ../build/build_fe.sh)
+(cd personal-site-frontend && sh ../build/test_fe.sh && sh ../build/build_fe.sh)
