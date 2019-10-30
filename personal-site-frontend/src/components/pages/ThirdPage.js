fec0868535910f49e4b09c386971a2446d5050e0
Julian Callin <j.callin@hotmail.com>
2019-10-29 18:44:56 -0700

    Add basic photo gallery

--- personal-site-frontend/src/components/pages/ThirdPage.js
+++ personal-site-frontend/src/components/pages/ThirdPage.js
@@ -1,38 +1,51 @@
 import React from "react";
 import Gallery from "react-grid-gallery";
 
+import Ted from "../../media/photography/ted.jpg";
+
 const IMAGES = [
   {
-    src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-    thumbnail:
-      "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "After Rain (Jeshu John - designerspics.com)"
+    src: Ted,
+    thumbnail: Ted,
+    thumbnailWidth: 650,
+    thumbnailHeight: 300,
+    caption: "Ted 'Dogg' climbing 'Mini Dihedral'"
   },
   {
-    src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-    thumbnail:
-      "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-    thumbnailWidth: 320,
-    thumbnailHeight: 212,
-    tags: [
-      { value: "Ocean", title: "Ocean" },
-      { value: "People", title: "People" }
-    ],
-    caption: "Boats (Jeshu John - designerspics.com)"
+    src: Ted,
+    thumbnail: Ted,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300,
+    caption: "Ted 'Dogg' climbing 'Mini Dihedral'"
   },
   {
-    src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-    thumbnail:
-      "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-    thumbnailWidth: 320,
-    thumbnailHeight: 212
+    src: Ted,
+    thumbnail: Ted,
+    thumbnailWidth: 650,
+    thumbnailHeight: 300,
+    caption: "Ted 'Dogg' climbing 'Mini Dihedral'"
+  },
+  {
+    src: Ted,
+    thumbnail: Ted,
+    thumbnailWidth: 650,
+    thumbnailHeight: 300,
+    caption: "Ted 'Dogg' climbing 'Mini Dihedral'"
   }
 ];
 
 const ThirdPage = () => {
-  return <Gallery images={IMAGES} enableImageSelection={false} />;
+  return (
+    <div className="third-page">
+      <p className="description">Here are some photos</p>
+      <Gallery
+        className="third-page"
+        images={IMAGES}
+        enableImageSelection={false}
+        rowHeight="300"
+      />
+    </div>
+  );
 };
 
 export default ThirdPage;
